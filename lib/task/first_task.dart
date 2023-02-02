399cf72be09dd633a3886af0e812e46f73c32f97
wyongjae <dnjsdydwo123@naver.com>
2023-02-02 23:07:17 +0900

    Refactor pages

---
+++ lib/task/first_task.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FirstTask extends StatelessWidget {
+  const FirstTask({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/task/second_task.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SecondTask extends StatelessWidget {
+  const SecondTask({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
