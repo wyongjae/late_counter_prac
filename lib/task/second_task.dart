ab29124f900386b58e1a01f9bc71a3a531d70f06
wyongjae <dnjsdydwo123@naver.com>
2023-02-02 23:29:31 +0900

    Feat ElevatedButton in second task page

--- lib/task/second_task.dart
+++ lib/task/second_task.dart
@@ -9,6 +9,20 @@ class SecondTaskPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Second Task Page'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            debugPrint('다운로드 시작!');
+            await Future.delayed(const Duration(seconds: 1));
+            debugPrint('초기화 중...');
+            await Future.delayed(const Duration(seconds: 1));
+            debugPrint('핵심 파일 로드 중...');
+            await Future.delayed(const Duration(seconds: 3));
+            debugPrint('다운로드 완료!');
+          },
+          child: const Text('print'),
+        ),
+      ),
     );
   }
 }
